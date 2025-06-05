3bdf4733a10e766195b067e5323edc24b75ab93a
anhviett1 <vietanh2206sl@gmail.com>
2025-06-05 12:06:51 +0700

    upload file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,16 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vite.dev/config/
-export default {
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
+    host: true,
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://backend:8000',
@@ -12,6 +20,11 @@ export default {
         rewrite: path => path.replace(/^\/api/, '')
       }
     }
-  }
-}
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
+})
 
